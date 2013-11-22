7c5fa249c990487fbee63028d03c2d14a2a859b3
geranperedo <zggperedo@globe.com.ph>
2013-11-22 19:15:28 +0800

    template dev

--- src/com/gp/aegen/main/SelectFormat.java
+++ src/com/gp/aegen/main/SelectFormat.java
@@ -1,20 +1,131 @@
 package com.gp.aegen.main;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class SelectFormat extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SelectFormat extends Activity{
+	Spinner firstfField;
+	Spinner secondField;
+	EditText midiator;
+	
+	ArrayAdapter<String> adapterFNField; 
+	ArrayAdapter<String> adapterSNField;
+	
+	String select_firstfField;
+	String select_secondField;
+	String select_midiator;
+	
+	List<String> c_list;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.selectformat);
 		
-		Spinner spinner1 = (Spinner) findViewById(R.id.fnspinner);
-		ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.assets_textfile_array, android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
-		adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); 
-		spinner1.setAdapter(adapter1); // Apply the adapter to the spinner
+		firstfField = (Spinner) findViewById(R.id.fnspinner);
+		secondField = (Spinner) findViewById(R.id.lnspinner);
+		midiator = (EditText) findViewById(R.id.midiator_editText);
+		Button btnFieldNext = (Button) findViewById(R.id.btn_nextId);
+		
+		c_list = new ArrayList<String>();
+		c_list.add("US - First Name");
+		c_list.add("US - Last Name");
+		c_list.add("Philippines - First Name");
+		c_list.add("philippines - Last Name");
+		c_list.add("UK - First Name");
+		c_list.add("UK - Last Name");
+		c_list.add("India - First Name");
+		c_list.add("India - Last Name");
+		c_list.add("Keyword");
+		
+		adapterFNField = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,c_list);
+		adapterFNField.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		firstfField.setAdapter(adapterFNField);
+		
+		adapterSNField = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,c_list);
+		adapterSNField.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		secondField.setAdapter(adapterSNField);
+		
+		
+		//Listening to button event
+		btnFieldNext.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View arg0) {
+            	
+            	select_firstfField  = firstfField.getSelectedItem().toString();
+            	select_secondField  = secondField.getSelectedItem().toString();
+            	select_midiator = midiator.getText().toString();
+            	
+                //Starting a new Intent
+                Intent nextScreen = new Intent(getApplicationContext(), SelectDomainName.class);
+ 
+                //Sending data to another Activity
+                nextScreen.putExtra("firstField", getValueFromHashMap(select_firstfField));
+                nextScreen.putExtra("secondField", getValueFromHashMap(select_secondField));
+                nextScreen.putExtra("midiator", select_midiator);
+ 
+                Log.e("Selected Format: ", getValueFromHashMap(select_firstfField)+""+select_midiator+""+getValueFromHashMap(select_secondField));
+ 
+                startActivity(nextScreen);
+ 
+            }
+        });
+		
+		firstfField.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+	    {
+
+			public void onItemSelected(AdapterView<?> arg0, View v, int position, long id)
+	        {
+				if(arg0.getItemAtPosition(position).toString().equals("Keyword")){
+					midiator.setEnabled(false);
+	            Log.v("routes", "route selected");
+				}else{
+					Log.v("routes", "fuck!");
+				}
+	        }
+
+	        public void onNothingSelected(AdapterView<?> arg0)
+	        {
+	            Log.v("routes", "nothing selected");
+	        }
+			
+	    });
 	}
+	
+	public String getValueFromHashMap(String value){
+				String textFileValue = null;
+		
+				//Using Hashmap
+				HashMap<String, String> hm = new HashMap<String, String>();
+				hm.put("Keyword", "null");
+				hm.put("US - First Name", "fname-us.txt");
+				hm.put("US - First Name", "fname-us.txt");
+				hm.put("US - Last Name", "lname-us.txt");
+				hm.put("Philippines - First Name", "fname-phil.txt");
+				hm.put("Philippines - Last Name", "lname-phil.txt");
+				hm.put("UK - First Name", "fname-uk.txt");
+				hm.put("UK - Last Name", "lname-uk.txt");
+				hm.put("India - First Name", "fname-inda.txt");
+				hm.put("India - Last Name", "lname-inda.txt");
+				
+				textFileValue = hm.get(value);
+				
+		return textFileValue;
+	}
+	
 }

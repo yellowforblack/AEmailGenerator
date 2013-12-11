a321089db7570b364a89b9b34571ad206e3cfdf1
geranperedo <zggperedo@globe.com.ph>
2013-12-11 17:38:43 +0800

    domainName: fixed domain name format with validation and alert box

--- src/com/gp/aegen/main/SelectDomainName.java
+++ src/com/gp/aegen/main/SelectDomainName.java
@@ -1,9 +1,13 @@
 package com.gp.aegen.main;
+import com.gp.aegen.model.DomainFormat;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class SelectDomainName extends Activity {
@@ -12,6 +16,10 @@ public class SelectDomainName extends Activity {
 	String secondField;
 	String keyword_editT;
 	String midiator;
+	EditText domainEditT;
+	
+	AlertDialogManager alert = new AlertDialogManager();
+	DomainFormat checkDomain = new DomainFormat();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,7 @@ public class SelectDomainName extends Activity {
 		setContentView(R.layout.select_domainname);
 		
 		Button nxtBtn = (Button) findViewById(R.id.next_btn);
+		domainEditT = (EditText) findViewById(R.id.domain_editText);
 		
 		Bundle extras = getIntent().getExtras();
 		
@@ -35,6 +44,22 @@ public class SelectDomainName extends Activity {
 				public void onClick(View v) {
 					 
 					Log.i("info", firstField+" "+secondField+" "+keyword_editT+" "+midiator);
+					//Starting a new Intent
+	                Intent nextScreen = new Intent(getApplicationContext(), SelectKeyWord.class);
+					
+					if(domainEditT.getText().toString().trim().equals("")){
+						alert.showAlertDialog(SelectDomainName.this, "Entry Failed...", "Domain name is Empty", false);
+					}else if(!checkDomain.validate(domainEditT.getText().toString().trim())){
+						alert.showAlertDialog(SelectDomainName.this, "Entry Failed...", "Invalid domain", false);
+					}else{
+						nextScreen.putExtra("firstField", firstField);
+	                    nextScreen.putExtra("secondField", secondField);
+	                    nextScreen.putExtra("keyword_editT", keyword_editT);
+	                    nextScreen.putExtra("midiator", midiator);
+	                    nextScreen.putExtra("domainName", domainEditT.getText().toString().trim());
+	                    
+	                    startActivity(nextScreen);
+					}
 				}
 		    	
 		    });
